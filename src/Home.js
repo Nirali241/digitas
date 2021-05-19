c17b6ab2a41af2c23d951f6be67d3f2a038b51f1
Nirali <bhalodiyanirali241@gmail.com>
2021-05-19 12:29:05 +0530

    commit

--- src/Home.js
+++ src/Home.js
@@ -4,11 +4,11 @@ import { Image } from 'react-bootstrap'
 function Home() {
     return (
         <div>
-            <Image style={{backgroundSize : 'cover', width: '100vw', height: '30vh'}} responsive  src="https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YnVzaW5lc3N8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80" fluid />
-            <div style={{position: 'absolute', backgroundColor: '#000000', opacity: '40%', width: '550px', height: '80px', top: '50%', left: '40%'}}>
+            <Image style={{backgroundSize : 'cover', width: '100vw', height: '40vh'}} responsive  src="https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YnVzaW5lc3N8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80" fluid />
+            <div style={{position: 'absolute', backgroundColor: '#000000', opacity: '40%', width: '550px', height: '80px', top: '30%', left: '40%'}}>
             </div>
             <p style={{ position: 'absolute', 
-            top: '50%', 
+            top: '30%', 
             left: '42%',
             background: 'tranparent',
             color: '#FFFFFF'
